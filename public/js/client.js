3c7bbc228a04b3468a6d2e9ad2fb029fa8ab798a
gkessman <greg.kessman@gmail.com>
2015-11-23 21:51:25 -0500

    Add Router

--- public/js/client.js
+++ public/js/client.js
@@ -24,13 +24,29 @@ var StudentView = Backbone.View.extend({
 });
 
 var StudentsView = Backbone.View.extend({
+	initialize: function() {
+		this.listenTo(this.collection, "reset", this.render);
+	},
 	tagName: "ul",
 	className: "students",
 	render: function() {
+		this.$el.html("");
 		this.collection.each(function(student) {
 			var studentView = new StudentView({ model: student });
 			this.$el.append(studentView.render().el);
 		}, this);
 		return this;
+	}
+});
+
+var AppRouter = Backbone.Router.extend({
+	routes: {
+		"": "index"
+	},
+	index: function() {
+		var collection = new Students();
+		collection.fetch({ reset: true });
+		var view = new StudentsView({ collection: collection });
+		$(".container").append(view.render().el);
 	}
 });
--- public/js/init.js
+++ public/js/init.js
@@ -1,9 +1,4 @@
 $(function() {
-	var collection = new Students();
-	collection.fetch({
-		success: function(data) {
-			var view = new StudentsView({ collection: data });
-			$(".container").append(view.render().el);
-		}
-	});
-})
+	new AppRouter();
+	Backbone.history.start();
+});
